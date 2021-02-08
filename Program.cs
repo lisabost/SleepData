adee6ff15b64a3c267aa42294d59daff7e50b6b4
Lisa Bost <bost.lisam@gmail.com>
2021-02-08 13:34:22 -0600

    Added NLog and error handling for invalid number of weeks entered

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using NLog.Web;
 
 namespace SleepData
 {
@@ -7,6 +8,12 @@ namespace SleepData
     {
         static void Main(string[] args)
         {
+            // create instance of Logger
+            string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog(path).GetCurrentClassLogger();
+
+            logger.Info("Program started");
+
             var file = "data.txt";
             // ask for input
             Console.WriteLine("Enter 1 to create data file.");
@@ -22,37 +29,44 @@ namespace SleepData
                 // ask a question
                 Console.WriteLine("How many weeks of data is needed?");
                 // input the response (convert to int)
-                int weeks = int.Parse(Console.ReadLine());
-
-                // determine start and end date
-                DateTime today = DateTime.Now;
-                // we want full weeks sunday - saturday
-                DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
-                // subtract # of weeks from endDate to get startDate
-                DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
-
-                // random number generator
-                Random rnd = new Random();
-
-                // create file
-                StreamWriter sw = new StreamWriter("data.txt");
-                // loop for the desired # of weeks
-                while (dataDate < dataEndDate)
+                string ans = Console.ReadLine();
+                int weeks;
+                if (!int.TryParse(ans, out weeks))
                 {
-                    // 7 days in a week
-                    int[] hours = new int[7];
-                    for (int i = 0; i < hours.Length; i++)
-                    {
-                        // generate random number of hours slept between 4-12 (inclusive)
-                        hours[i] = rnd.Next(4, 13);
-                    }
-                    // M/d/yyyy,#|#|#|#|#|#|#
-                    //Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
-                    sw.WriteLine($"{dataDate:M/d/yyyy},{string.Join("|", hours)}");
-                    // add 1 week to date
-                    dataDate = dataDate.AddDays(7);
+                    logger.Error("Invalid input (integer): {Answer}", ans);
                 }
-                sw.Close();
+                else
+                {
+                    // determine start and end date
+                    DateTime today = DateTime.Now;
+                    // we want full weeks sunday - saturday
+                    DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
+                    // subtract # of weeks from endDate to get startDate
+                    DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
+
+                    // random number generator
+                    Random rnd = new Random();
+
+                    // create file
+                    StreamWriter sw = new StreamWriter(file);
+                    // loop for the desired # of weeks
+                    while (dataDate < dataEndDate)
+                    {
+                        // 7 days in a week
+                        int[] hours = new int[7];
+                        for (int i = 0; i < hours.Length; i++)
+                        {
+                            // generate random number of hours slept between 4-12 (inclusive)
+                            hours[i] = rnd.Next(4, 13);
+                        }
+                        // M/d/yyyy,#|#|#|#|#|#|#
+                        //Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+                        sw.WriteLine($"{dataDate:M/d/yyyy},{string.Join("|", hours)}");
+                        // add 1 week to date
+                        dataDate = dataDate.AddDays(7);
+                    }
+                    sw.Close();
+                }
             }
             else if (resp == "2")
             {
@@ -85,12 +99,13 @@ namespace SleepData
                         string[] days = { "Mo", "Tu", "We", "Th", "Fr", "Sa", "Su", "Tot", "Avg" };
                         Console.WriteLine($"{days[0],3} {days[1],3} {days[2],3} {days[3],3} {days[4],3} {days[5],3} {days[6],3} {days[7],4} {days[8],4}");
                         Console.WriteLine(" --  --  --  --  --  --  --  ---  ---");
-                        Console.WriteLine($"{hoursSleep[0],3} {hoursSleep[1],3} {hoursSleep[2],3} {hoursSleep[3],3} {hoursSleep[4],3} {hoursSleep[5],3} {hoursSleep[6],3} {total,4} {average, 4:n1}");
+                        Console.WriteLine($"{hoursSleep[0],3} {hoursSleep[1],3} {hoursSleep[2],3} {hoursSleep[3],3} {hoursSleep[4],3} {hoursSleep[5],3} {hoursSleep[6],3} {total,4} {average,4:n1}");
                         Console.WriteLine("");
                     }
                 }
 
             }
+            logger.Info("Program ended");
         }
     }
 }
